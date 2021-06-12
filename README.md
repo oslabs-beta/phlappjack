ad81ebca943405d9eb12e3e59ddbe0bce3334744
aps1226 <79802526+aps1226@users.noreply.github.com>
2021-06-12 09:25:32 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# phlappjack-node
+
+run 
+`npm install`
+`npm run dev`
+
+MongoDB basic functionallity has been completed. Schema's can be created and deleted by selecting the respective names in the 'Existing Models' display. Upon selection of a schema, input fields can be added and deleted. To delete either a schema or an input field, hold the selection down until the selection is completely colored with red.
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const cors = require("cors");
+const bodyParser = require('body-parser')
+const AppConfigurator  = require('./AppConfiguration/appConfigurator.js')
+const port = 3000
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(path.dirname(__dirname), './client/')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client", "index.html"));
+})
+
+app.post("/ConfigureApplication", (req, res, next) => {
+  props = req.body;
+  const { newApplication, atlasHostCluster, atlasUserName, atlasPassword, atlasDB, dbInputDisplay, dockerFile, dockerCompose } = props;
+  AppConfigurator(newApplication, atlasHostCluster, atlasUserName, atlasPassword, atlasDB, dbInputDisplay, dockerFile, dockerCompose );
+  next();
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
