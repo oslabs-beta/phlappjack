ff80aead1e1ad6233c2fdac3a41a26b23fdd5c5e
aps1226 <79802526+aps1226@users.noreply.github.com>
2021-06-12 09:20:22 -0400

    Delete createBundle.ts

--- createBundle.ts
+++
@@ -1,9 +0,0 @@
-const createBundle = async () => {
-  const { files, diagnostics } = await Deno.emit("./client/index.tsx", {
-    bundle: "module",
-  });
-
-  await Deno.writeTextFile("./build/bundle.js", files["deno:///bundle.js"]);
-};
-
-export { createBundle };
