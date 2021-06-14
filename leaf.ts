5e409739a8c4ceab7de62f227309a21bd3026add
nonstdout <csalisbury@natilik.com>
2021-06-14 22:10:50 +0100

    testing with forked leaf repo

--- leaf.ts
+++ leaf.ts
@@ -2,5 +2,6 @@ import { Leaf } from "./deps.ts";
 
 Leaf.compile({
   modulePath: "./mod.ts",
-  contentFolders: ["./build"]
+  contentFolders: ["./build"],
+  flags: ["--allow-all", "--unstable"]
 })
